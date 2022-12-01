d55e6b7817d9cde25f4f2cd377ebf93f4bfa4b31
IceOnFire <antony.mistretta@gmail.com>
2022-12-01 14:58:01 +0100

    Create reducers for todo-list app

---
+++ todo-list/src/07-redux/todo-list/text.js
@@ -0,0 +1,16 @@
+const SET_TEXT = "set-text";
+
+export const setText = (text) => ({
+  type: SET_TEXT,
+  payload: text,
+});
+
+export default function text(state = "", action) {
+  switch (action.type) {
+    case SET_TEXT:
+      return action.payload;
+
+    default:
+      return state;
+  }
+}
